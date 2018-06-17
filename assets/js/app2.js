06b5cfe61dec2941ba20473bbc895018dae8272f
Paradox <cdesai516@gmail.com>
2018-06-17 09:28:09 -0400

    HomeworkGIfy

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -2,28 +2,16 @@
 
 $(document).ready(function() {
 
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
+  $('#particle-button').click(function(){
+    particlesJS.load('particle-js',"assets\particles.js\particles.json")
+});
 
 
 
 
-const gifyArry = ["fintech","digibyte","etherum","trading","blockchain","space","science","economics","bitcoin"]
+
+
+const gifyArry = ["fintech","digibyte","ethereum","Technology","blockchain","space","science","economics","bitcoin"]
 
     //button creation for each item 
     gifyArry.forEach(function(itemName){
